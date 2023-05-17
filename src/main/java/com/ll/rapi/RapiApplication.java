bcc6f356576a4eddf8ccea993b7c859037a24a5b
Youngwoo Choi <yw823x@daum.net>
2023-05-17 14:55:48 +0900

    POST

--- src/main/java/com/ll/rapi/RapiApplication.java
+++ src/main/java/com/ll/rapi/RapiApplication.java
@@ -2,8 +2,10 @@ package com.ll.rapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RapiApplication {
 
 	public static void main(String[] args) {
