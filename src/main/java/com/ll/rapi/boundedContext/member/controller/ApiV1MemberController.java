206d5adeceadc0bee5eb9f8cdec8cfce3cd80747
Youngwoo Choi <yw823x@daum.net>
2023-05-18 10:21:27 +0900

    RsData

---
+++ src/main/java/com/ll/rapi/boundedContext/member/controller/ApiV1MemberController.java
@@ -0,0 +1,65 @@
+package com.ll.rapi.boundedContext.member.controller;
+
+import com.ll.rapi.base.rsData.RsData;
+import com.ll.rapi.boundedContext.member.entity.Member;
+import com.ll.rapi.boundedContext.member.service.MemberService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.MediaType.ALL_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/api/v1/member", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+public class ApiV1MemberController {
+    private final MemberService memberService;
+
+    @Data
+    public static class LoginRequest {
+        @NotBlank
+        private String username;
+        @NotBlank
+        private String password;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class LoginResponse {
+        private final String accessToken;
+    }
+
+    @PostMapping("/login")
+    public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
+        String accessToken = memberService.genAccessToken(loginRequest.getUsername(), loginRequest.getPassword());
+
+        return RsData.of(
+                "S-1",
+                "엑세스 토큰이 생성되었습니다.",
+                new LoginResponse(accessToken)
+        );
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class MeResponse {
+        private final Member member;
+    }
+
+    // consumes = ALL_VALUE => 나는 딱히 JSON 을 입력받기를 고집하지 않겠다.
+    @GetMapping(value = "/me", consumes = ALL_VALUE)
+    public RsData<MeResponse> me() {
+        Member member = memberService.findByUsername("user1").orElse(null);
+
+        return RsData.of(
+                "S-1",
+                "성공",
+                new MeResponse(member)
+        );
+    }
+}
--- src/test/java/com/ll/rapi/RapiApplicationTest.java
+++ src/test/java/com/ll/rapi/RapiApplicationTest.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.nio.charset.StandardCharsets;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,7 +35,7 @@ class MemberControllerTest {
         // When
         ResultActions resultActions = mvc
                 .perform(
-                        post("/member/login")
+                        post("/api/v1/member/login")
                                 .content("""
                                         {
                                             "username": "user1",
@@ -57,7 +58,24 @@ class MemberControllerTest {
         MvcResult mvcResult = resultActions.andReturn();
 
         MockHttpServletResponse response = mvcResult.getResponse();
+    }
 
+    @Test
+    @DisplayName("GET /member/me 는 내 정보를 조회하는 URL 이다.")
+    void t2() throws Exception {
+        // When
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/member/me")
+                )
+                .andDo(print());
 
+        // Then
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.resultCode").value("S-1"))
+                .andExpect(jsonPath("$.msg").exists())
+                .andExpect(jsonPath("$.data.member.id").exists())
+                .andExpect(jsonPath("$.data.member.username").exists());
     }
 }
